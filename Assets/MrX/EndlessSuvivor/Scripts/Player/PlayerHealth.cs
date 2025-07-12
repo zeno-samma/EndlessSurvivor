ae5a72729ce9f06a424ddf6e4c09b739535630ee
Xuyen <tranvanxuyen111@gmail.com>
2025-07-12 10:16:35 +0700

    Upload file

---
+++ Assets/MrX/EndlessSuvivor/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace MrX.EndlessSurvivor
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        // === DỮ LIỆU ===
+        [SerializeField] private float maxHealth = 100f;
+        public PlayerConfigSO playerConfig; //
+        private float currentHealth;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
